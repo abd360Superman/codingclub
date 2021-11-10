2ffaae3bbbdad0102edd004622b41ac6db5834c1
abd360superman <78216399+abd360Superman@users.noreply.github.com>
2021-11-10 14:19:18 +0530

    Add files via upload

---
+++ Session 10/send2trash_funcs.py
@@ -0,0 +1,9 @@
+### import send2trash
+# Look at video attached on how to install it, as this is a third party module
+import send2trash
+
+# Send a file to the trash
+send2trash.send2trash("test.txt")
+
+# Send a folder to the trash
+send2trash.send2trash("test")
